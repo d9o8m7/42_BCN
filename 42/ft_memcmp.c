2a8580e312c7e878c503f3adae804b27a10dd0e3
Dan Oliver Monells <daoliver@cbr13s5.42barcelona.com>
2023-05-05 16:14:43 +0200

    Make worrks ft_strlen.c needs correction

---
+++ 42/ft_memcmp.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daoliver <daoliver@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 16:40:10 by daoliver          #+#    #+#             */
+/*   Updated: 2023/05/04 14:34:15 by daoliver         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//Compare firsts n bytes of area of memory str1 & str2
+//str1 first pointer to a block of memory.
+//str2 second pointer to a block of memory.
+//n (Number of bytes to compare)
+//RETURN VALUES
+//v < 0 str1 in minor to str2
+//v > 0 str1 is mayor to str2
+//v = 0 str1 is equal to str2
+
+#include "libft.h"
+
+int	ft_memcmp(const void *str1, const void *str2, size_t n)
+{
+	const unsigned char	*s1;
+	const unsigned char	*s2;
+	size_t	i;
+
+	s1 = str1;
+	s2 = str2;
+	i = 0;
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+			i++;
+	}
+	return (0);
+}
+
+/*int	main(void)
+{
+	int	res;
+	char str1[] = "DA-Maker";
+	char str2[] = "DA-mAKER";
+
+	res = ft_memcmp(str1, str2, 2);
+	printf("res: %d\n", res);
+}*/
--- 42/libft.h
+++ 42/libft.h
@@ -6,7 +6,7 @@
 /*   By: daoliver <daoliver@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 18:20:30 by daoliver          #+#    #+#             */
-/*   Updated: 2023/05/05 12:55:02 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/05/05 16:05:02 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 # include <unistd.h>
 
 //int	ft_atoi(const char *str);
-//void	ft_bezero(void *s, size_t n);
-//void	*ft_calloc(size_t count, size_t size);
+//void	ft_bzero(void *s, size_t n);
+//void	*ft_calloc(size_t cunt, size_t size):
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
 int	ft_isascii(int c);
@@ -26,5 +26,6 @@ int	ft_isdigit(int c);
 int	ft_isprint(int c);
 void	*ft_memchr(const void *str, int c, size_t n);
 int	ft_memcmp(const void *str1, const void *str2, size_t n);
+int	ft_strlen(const char *s);
 
 #endif
