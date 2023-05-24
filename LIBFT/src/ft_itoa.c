8db46d68d187f9c551180038a9af26fc2d5f940b
Dan Oliver Monells <daoliver@car5s2.42barcelona.com>
2023-05-24 13:14:26 +0200

    itoa undestanding

--- LIBFT/src/ft_itoa.c
+++ LIBFT/src/ft_itoa.c
@@ -6,13 +6,36 @@
 /*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 09:15:13 by daoliver          #+#    #+#             */
-/*   Updated: 2023/05/24 09:15:53 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/05/24 13:14:05 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(intn)
+static int	ft_len(int n)
 {
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n != 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*res;
+	int	len;
+	int	neg;
+
+	if (n = -2147483648)
+		return (ft_strdup("-2147483648"));
+	neg = 0;
+	len = ft_len(n);
 
 }
