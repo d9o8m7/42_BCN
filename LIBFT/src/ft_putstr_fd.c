3739c9e98adc507334c98968eb7fd07eef7842b7
Dan Oliver Monells <daoliver@cbr9s6.42barcelona.com>
2023-05-26 14:07:33 +0200

    nice friday

--- LIBFT/src/ft_putstr_fd.c
+++ LIBFT/src/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 09:19:26 by daoliver          #+#    #+#             */
-/*   Updated: 2023/05/24 09:20:03 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/05/26 12:53:17 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-
+	if (!s)
+		write(fd, s, ft_strlen(s));
 }
+
+/*int	main(void)
+{
+	ft_putstr_fd("52 Barcelona", 1);
+	ft_putstr_fd("\n", 1);
+}*/
--- LIBFT/src/ft_strmapi.c
+++ LIBFT/src/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 09:16:35 by daoliver          #+#    #+#             */
-/*   Updated: 2023/05/24 09:17:22 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/05/26 14:06:58 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char			*str;
+	unsigned int	i;
 
+	i = 0;
+	if (s == NULL || f == NULL)
+		return (0);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
+
+int	main(void)
+{
+	char	str
