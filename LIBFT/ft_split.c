1d5455290c6ae9b4db2b94b7b6018b73c5887e4b
D-O-M <dan.olivermonells@icloud.com>
2023-06-14 16:55:52 +0200

    Split 2/4

--- LIBFT/ft_split.c
+++ LIBFT/ft_split.c
@@ -6,13 +6,42 @@
 /*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 09:12:57 by daoliver          #+#    #+#             */
-/*   Updated: 2023/05/24 09:14:51 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/06/14 15:00:38 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+int	ft_len_words(char const *s, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0' && s[len] != c)
+		len++;
+	return (len);
+}
+
+int	ft_count_words(char const *s, char c)
+{
+	int	len_words;
+	int	i;
+
+	i = 0;
+	len_words = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			len_words++;
+		while (s[i] != c && s[i])
+			i++;
+	}
+	return (len_words);
+}
+
+/*char	**ft_split(char const *s, char c)
 {
 
-}
+}*/
