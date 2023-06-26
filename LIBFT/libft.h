5245c563e7aa3212ae98bed3ec360160928cc937
Dan Oliver Monells <daoliver@cbr14s5.42barcelona.com>
2023-06-26 13:36:07 +0200

    bonus

--- LIBFT/libft.h
+++ LIBFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: daoliver <daoliver@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 18:20:30 by daoliver          #+#    #+#             */
-/*   Updated: 2023/06/15 18:16:31 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/06/26 13:35:52 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,23 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+
+//void	ft_lstadd_back_bonus(t_list **lst, t_list *new);
+//void	ft_lstadd_front_bonus(t_list **lst, t_list *new);
+//void	ft_lstclear_bonus(t_list **lst, void (*del)(void *));
+//void	ft_lstdelone_bonus(t_list *lst, void (*del)(void *));
+//void	ft_lstiter_bonus(t_list **lst, void (*f)(void *));
+//t_list	ft_lstlast_bonus(t_list *lst);
+//t_list	*ft_lstmap_bonus(t_list *lst, void *(*f)(void *), void (*del)(void *);
+//t_list	*ft_lstnew_bonus(void *content);
+//int	ft_lstsize_bonus(t_list *lst);
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
@@ -25,15 +42,6 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 char	*ft_itoa(int n);
-//void	ft_lstadd_back_bonus(t_list **lst, t_list *new);
-//void	ft_lstadd_front_bonus(t_list **lst, t_list *new);
-//void	ft_lstclear_bonus(t_list **lst, void (*del)(void *));
-//void	ft_lstdelone_bonus(t_list *lst, void (*del)(void *));
-//void	ft_lstiter_bonus(t_list **lst, void (*f)(void *));
-//t_list	ft_lstlast_bonus(t_list *lst);
-//t_list	*ft_lstmap_bonus(t_list *lst, void *(*f)(void *), void (*del)(void *);
-//t_list	*ft_lstnew_bonus(void *content);
-//int	ft_lstsize_bonus(t_list *lst);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_memcmp(const void *str1, const void *str2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
