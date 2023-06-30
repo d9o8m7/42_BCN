b0af40d11e465d7c414e42ccfce805522afa888a
Dan Oliver Monells <daoliver@cbr15s6.42barcelona.com>
2023-06-30 18:37:32 +0200

    check

--- LIBFT/ft_memmove.c
+++ LIBFT/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: daoliver <daoliver@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 16:32:03 by daoliver          #+#    #+#             */
-/*   Updated: 2023/06/30 18:18:54 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/06/30 18:27:14 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,3 +38,14 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	}
 	return (dst);
 }
+
+/*int	main(void)
+{
+	char	dst[] = "Art is comming from the future";
+	char	src[] = "52 is builing the future";
+	size_t	n;
+
+	n = 6;
+	ft_memmove(dst, src, n);
+	printf("main:\n dst: %s\n len: %zu\n", dst, src, n);
+}*/
